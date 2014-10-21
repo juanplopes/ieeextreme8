6c294796cb86df02b1b55dd3b5c47e3ad719aed8
Juan Lopes <me@juanlopes.net>
2014-10-21 18:40:12 -0200

    18 and 21

--- 18-playing-with-tries.cpp
+++ 18-playing-with-tries.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
@@ -7,37 +6,40 @@ using namespace std;
 
 #define ull unsigned long long
 
-ull t, a, l, n;
+ull pow2(int a, int b) {
+    if (b==0) return 1;
+    if (b==1) return a;
+    ull r = pow2(a, b/2);
+    return r*r*pow2(a,b%2);
+}   
 
-int main()
-{
+int find(int a, int l, ull n) {
+    int first=0, last=l+1;
+    int count = last-first;
+    while (count>0) {
+        int it = first, step=count/2; it+=step;
+        
+        ull p = pow2(a, it);
+        if (p!=0 and pow2(a, it)<n) {
+            first=++it;
+            count-=step+1;
+        } else count=step;
+    }
+    return first;
+}
+
+int main() {
+	int t, a, l;
+	ull n;
 	cin >> t;
-	while( t-- )
+	while(cin >> a >> l >> n)
 	{
-		scanf( "%llu %llu %llu", &a, &l, &n );
-		//cin >> a >> l >> n;
-		
-		ull link = 1;
-		ull pot = 1;
-	
-		
-		for( int i = 0; i < l; i++ )
-		{
-			pot *= a;
-			if( pot < n )
-			{
-				link += pot;
-			}
-			else
-			{
-			    link += ( l-i ) * n;
-			    break;
-			}
-		}
-		
-		
-		printf( "%llu\n", link * a * 4 );
-		//cout <<  << endl;
+	    ull link = 1+l;
+        if (a>1) {
+            int i=find(a, l, n)-1;
+            link = (pow2(a, i+1)-1)/(a-1) + (l-i)*n;
+        }
+		cout << link * a * 4 << endl;
 	}
 
 	return 0;
--- 21-star-fleet-express.cpp
+++ 21-star-fleet-express.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <cstring>
 #define ERR (1ll<<63-1)   
 #define ull unsigned long long
 using namespace std;
@@ -14,8 +15,12 @@ struct Flight {
 
 map<string, int> M;
 vector<Flight> G[1010];
+ull best[1010];
 
 ull dfs(int path, int from, int dest, ull now) {
+    if (now>=best[from]) return ERR;
+    best[from] = now;
+        
     if (from==dest) return now;
     ull minn = ERR;
     for(int i=0; i<G[from].size(); i++) {
@@ -36,17 +41,18 @@ int main() {
         for(int i=0; i<m; i++) {
             string a, b; ull a1, a2, b1, b2;
             cin >> a >> a1 >> a2 >> b >> b1 >> b2;
-            G[M[a]].push_back(Flight(M[b], a1*1400+a2, b1*1400+b2));
+            G[M[a]].push_back(Flight(M[b], a1*1440+a2, b1*1440+b2));
         }
         
         for(int i=0; i<q; i++) {
             string a, b; ull a1, a2;
             cin >> a >> a1 >> a2 >> b;
-            ull answer = dfs(0, M[a], M[b], a1*1400+a2);
+            memset(best, 0x3f, sizeof best);
+            ull answer = dfs(0, M[a], M[b], a1*1440+a2);
             if (answer == ERR) {
                 cout << "No trip on XSL" << endl;
             } else {
-                cout << answer/1400 << " " << answer%1400 << endl;
+                cout << answer/1440 << " " << answer%1440 << endl;
             }
         }
     }
